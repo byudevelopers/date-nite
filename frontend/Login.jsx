ce8da1ca09b19f3b332aed73c1a91f6c9be17a45
AudreyBurrell <audreyb2165@gmail.com>
2026-02-12 20:09:32 -0700

    add pages

---
+++ frontend/Login.jsx
@@ -0,0 +1,32 @@
+//css statement goes here
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  return (
+    <div>
+      <h1>Login to Date-Nite</h1>
+      <div className="formArea">
+        <form method="get">
+          <div className="emailInput">
+            <input className="form-control" type="email" placeholder="Email" />
+          </div>
+          <div className="passwordInput">
+            <input className="form-control" type="password" placeholder="Password" />
+          </div>
+        </form>
+        <div className="controlBtns">
+          <button className="submitBtn" type="submit">Submit</button>
+          <button className="createAccountBtn" type="submit">Create Account</button>
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
+
+//After login the user goes here
+
+
+
+export default Login;
