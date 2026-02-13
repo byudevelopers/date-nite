5967d658e4b39a957c4d302d195a40261256b02a
michelle-johanson <mjohans0@byu.edu>
2026-02-12 20:03:04 -0700

    shame

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,19 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div>
-        <h1>Welcome to Date Nite!</h1>
-        <Router>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/about" element={<About />} /> */}
-            {/* Add more routes as needed */}
-        </Routes>
-        </Router>
-    </div>
-
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        {/* <Route path="about" element={<About />} /> */}
+      </Route>
+    </Routes>
   );
 }
 
