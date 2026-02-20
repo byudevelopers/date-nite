d0be586fe9dbd5dd25479e4ff5047d8471af9059
michelle-johanson <mjohans0@byu.edu>
2026-02-19 22:42:03 -0700

    add files and folders to src/

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Favorites from './pages/Favorites';
 
 function App() {
   return (
     <div>
         <h1>Welcome to Date Nite!</h1>
         <Routes>
-            <Route path="/" element={<Login />} />
-            {/* <Route path="/about" element={<About />} /> */}
-            {/* Add more routes as needed */}
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
     </div>
 
