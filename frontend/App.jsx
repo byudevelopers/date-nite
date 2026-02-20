368d38c786a6519b27c34b34dd5b1a22ad5ed6ec
AudreyBurrell <audreyb2165@gmail.com>
2026-02-19 19:24:16 -0700

    Getting it to work

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
+import Home from './Home';
 import React from 'react';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
               <Route path="/" element={<Login />} />
               {/* <Route path="/about" element={<About />} /> */}
               {/* Add more routes as needed */}
+              <Route path="/Home" element={<Home />}/>
           </Routes>
         </Router>
     </div>
---
+++ frontend/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+function Login() {
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('');
+  }
+
+  return (
+    <div>
+      <h1>Login to Date-Nite</h1>
+      <div className="formArea">
+        <form method="get">
+          <div className="emailInput">
+            <input className="form-control" type="email" placeholder="Email" />
+          </div>
+          <div className="passwordInput">
+            <input className="form-control" type="password" placeholder="Password" />
+          </div>
+        </form>
+        <div className="controlBtns">
+          <button className="submitBtn" type="submit">Submit</button>
+          <button className="createAccountBtn" type="submit">Create Account</button>
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
+
+
+
+
+export default Login;
