da7547455e7d6ad6d8b48067222953c3bf17fcd6
hayleecrow <cro21045@byui.edu>
2026-02-26 20:35:17 -0700

    Added bootstap formatting to login elements

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,26 +1,29 @@
 import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import App from '../App';
+import '../styles/login.css';
 
 function Login() {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <main className="container-fluid">
       <h1>Login to Date-Nite</h1>
       <div className="formArea">
-        <form>
-          <div className="emailInput">
+        <form method="get">
+          <div className="emailInput input-group mb-3">
             <input className="form-control" type="email" placeholder="Email" />
           </div>
-          <div className="passwordInput">
+          <div className="passwordInput input-group mb-3">
             <input className="form-control" type="password" placeholder="Password" />
           </div>
         </form>
         <div className="controlBtns">
-          <button className="submitBtn" onClick={() => navigate('/home')}>Submit</button>
-          <button className="createAccountBtn" onClick={() => navigate('/home')}>Create Account</button>
+          <button className="submitBtn btn btn-primary" onClick={() => navigate('/home')}>Submit</button>
+          <button className="createAccountBtn btn" onClick={() => navigate('/home')}>Create Account</button>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
 
