2de42352b55fe24618a7d2ed8bbe2a702538d2c7
hayleecrow <cro21045@byui.edu>
2026-03-19 20:06:22 -0600

    Corrected navbar import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
-import Navbar from './pages/components/navbar';
+import Navbar from './pages/components/Navbar';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
